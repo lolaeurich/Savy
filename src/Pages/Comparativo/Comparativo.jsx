939f515bce063eb42bded45ca29195ab779d0c72
unknown <lola.tatti@gmail.com>
2024-07-16 21:01:28 -0300

    feat: cards mercados

--- src/Pages/Comparativo/Comparativo.jsx
+++ src/Pages/Comparativo/Comparativo.jsx
@@ -4,6 +4,7 @@ import cart from "../../Assets/cart.png";
 import flecha from "../../Assets/flecha-esquerda.png";
 import mercado from "../../Assets/home.png";
 import produtos from "../../Assets/produtos.png";
+import local from "../../Assets/local-cinza.png";
 
 function Comparativo () {
     return (
@@ -45,11 +46,57 @@ function Comparativo () {
                     </div>
                 
                     <div className="card2">
-
+                        <h3>Melhor custo-benefício em um só lugar</h3>
+                        <div className="lista-mercados">
+                            <div className="mercado1">
+                                <p className="mercado-distancia">100 M</p>
+                                <img alt="" src={mercado} />
+                                <p className="mercado-produtos">XX produtos</p>
+                                <p className="mercado-economia">R$ 00.00</p>
+                            </div>
+                            <div className="mercado1">
+                                <p className="mercado-distancia">100 M</p>
+                                <img alt="" src={mercado} />
+                                <p className="mercado-produtos">XX produtos</p>
+                                <p className="mercado-economia">R$ 00.00</p>
+                            </div>
+                            <div className="mercado1">
+                                <p className="mercado-distancia">100 M</p>
+                                <img alt="" src={mercado} />
+                                <p className="mercado-produtos">XX produtos</p>
+                                <p className="mercado-economia">R$ 00.00</p>
+                            </div>
+                            <div className="mercado1">
+                                <p className="mercado-distancia">100 M</p>
+                                <img alt="" src={mercado} />
+                                <p className="mercado-produtos">XX produtos</p>
+                                <p className="mercado-economia">R$ 00.00</p>
+                            </div>
+                        </div>
+                        <div className="card1-btns">
+                            <button className="ver-mercados">Ver supermercados</button>
+                            <button className="custo">Custo R$ 00,00</button>
+                        </div>
                     </div>
 
                     <div className="card3">
+                        <h3>Savvyr</h3>
+                        <p>Inteligência, qualidade e conveniência na sua porta</p>
+                            <div className="card3-icons">
+                                <div className="icon3">
+                                    <img className="icon3-img" alt="" src={mercado} />
+                                    <p className="icon3-p">Receba em casa</p>
+                                </div>
 
+                                <div className="icon4">
+                                    <img className="icon4-img" alt="" src={produtos} />
+                                    <p className="icon4-p">Todos os produtos</p>
+                                </div>
+                            </div>
+                        <div className="card1-btns">
+                            <button className="ver-mercados">Ver supermercados</button>
+                            <button className="custo">Custo R$ 00,00</button>
+                        </div>    
                     </div>
                 </div>    
             </div>    
